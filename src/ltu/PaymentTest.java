4e54b31d67c8f8b52723fa0dd4ec79e10253364b
bennerosen <bernhard.rosenzweig123@gmail.com>
2026-09-10 14:27:14 +0200

    Add payment boundary tests and ignore generated files

--- src/ltu/PaymentTest.java
+++ src/ltu/PaymentTest.java
@@ -1,15 +1,53 @@
 package ltu;
+import java.io.IOException;
+import java.util.Date;
 
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+
+
+
 public class PaymentTest
 {
-    @Test
-    public void testSilly()
+    private static class FixedCalendar implements ICalendar
     {
-        assertEquals(1, 1);
+        public Date getDate(){
+            return new Date ("25/05/2016");
+        }
+    }
+ 
+
+    @Test
+    public void testUnder20GetsNoPayment() throws IOException
+    {
+        PaymentImpl payment = new PaymentImpl(new FixedCalendar());
+
+        assertEquals(0, payment.getMonthlyAmount("19970202-0000", 0, 100, 100));
+    }
+
+    @Test
+    public void test20AndOlderGetsPayment() throws IOException
+    {
+        PaymentImpl payment = new PaymentImpl(new FixedCalendar());
+        
+        assertEquals(9904, payment.getMonthlyAmount("19960202-0000", 0, 100, 100));
+    }
+
+    @Test
+    public void test49PercentCompltetion() throws IOException
+    {
+        PaymentImpl payment = new PaymentImpl(new FixedCalendar());
+        
+        assertEquals(0, payment.getMonthlyAmount("19960202-0000", 0, 100, 49));
+    }
+    @Test
+    public void test50PercentCompltetion() throws IOException
+    {
+        PaymentImpl payment = new PaymentImpl(new FixedCalendar());
+        
+        assertEquals(9904, payment.getMonthlyAmount("19960202-0000", 0, 100, 50));
     }
 
 }
